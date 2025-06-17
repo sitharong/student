f003bf1b2031fc4ad890324e55b3adfccac1ee87
sitharong <sitharong.work@gmail.com>
2025-06-17 10:44:06 +0700

    clean

--- src/main/java/com/acleda/student/controllers/StudentRestController.java
+++ src/main/java/com/acleda/student/controllers/StudentRestController.java
@@ -11,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,14 +27,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Slf4j
 public class StudentRestController {
 
-    private static final Logger logger = LoggerFactory.getLogger(StudentRestController.class);
-
     @Autowired
     StudentService studentService;
 
     @GetMapping
     public String helloWorld() {
-        logger.info("helloWorld");
+        log.info("helloWorld");
         return "Hello World";
     }
 
