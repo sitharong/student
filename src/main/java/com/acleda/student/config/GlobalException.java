7e0fe8c0b96cc018a47f34fb3bac7eaece91bb42
sitharong <sitharong.work@gmail.com>
2025-06-19 08:03:12 +0700

    clean

--- src/main/java/com/acleda/student/config/GlobalException.java
+++ src/main/java/com/acleda/student/config/GlobalException.java
@@ -2,6 +2,8 @@ package com.acleda.student.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,23 +15,28 @@ import java.util.NoSuchElementException;
 @ControllerAdvice
 public class GlobalException {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Object methodArgumentNotValidException(MethodArgumentNotValidException e,
-            HttpServletRequest request) {
-        log.error("{} {} {} {}", HttpStatus.BAD_REQUEST, request.getMethod(), request.getRequestURL(),
-                e.getBindingResult().getAllErrors());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getBindingResult().getAllErrors());
+    @ExceptionHandler
+    public Object methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        log.error("{} {} {} {}", HttpStatus.BAD_REQUEST, request.getMethod(), request.getRequestURL(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
-    @ExceptionHandler(NoSuchElementException.class)
+    @ExceptionHandler
     public Object noSuchElementException(NoSuchElementException e, HttpServletRequest request) {
         log.error("{} {} {} {}", HttpStatus.NOT_FOUND, request.getMethod(), request.getRequestURL(), e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
-    @ExceptionHandler(Exception.class)
+    @ExceptionHandler
+    public Object dataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
+        log.error("{} {} {} {}", HttpStatus.CONFLICT, request.getMethod(), request.getRequestURL(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler
     public Object exception(Exception e, HttpServletRequest request) {
-        log.error("{} {} {} {}", HttpStatus.INTERNAL_SERVER_ERROR, request.getMethod(), request.getRequestURL(), e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+        log.error("{} {} {} {} {}", HttpStatus.INTERNAL_SERVER_ERROR, request.getMethod(), request.getRequestURL(),
+                e.getClass(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
     }
 }
