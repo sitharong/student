6d00e0403fbb2a3a56449f6392357ed7f571515f
sitharong <sitharong.work@gmail.com>
2025-06-19 16:25:57 +0700

    c

--- src/main/java/com/acleda/student/jwt/UserModel.java
+++ src/main/java/com/acleda/student/jwt/UserModel.java
@@ -1,9 +1,11 @@
 package com.acleda.student.jwt;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "tbl_user")
+@Data
 public class UserModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,38 +17,7 @@ public class UserModel {
     @Column(nullable = false)
     private String password;
 
-    private String role = "USER";
+    @Column(nullable = false)
+    private String role;
 
-    // Getters and setters
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
