e1c9d35765a8d10550af0d4517c4f39d23e4dbfd
sitharong <sitharong.work@gmail.com>
2025-06-23 11:59:30 +0700

    c

--- src/main/java/com/acleda/student/config/GlobalExceptionHandler.java
+++ src/main/java/com/acleda/student/config/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.acleda.student.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.acleda.student.response.ResponseService;
 
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Slf4j
 @ControllerAdvice
@@ -23,29 +23,41 @@ public class GlobalExceptionHandler {
     @Autowired
     private ResponseService responseService;
 
-    private Object handler(Exception e, HttpServletRequest request, HttpStatus s) {
-        // log.error("{} {} {} {} {}", s, request.getMethod(), request.getRequestURL(),
-        // e.getClass(), e.getMessage());
-        return ResponseEntity.status(s).body(responseService.error(e.getMessage(), s));
-    }
-
-    @ExceptionHandler({ MethodArgumentNotValidException.class, ConstraintViolationException.class })
-    public Object httpStatus400(Exception e, HttpServletRequest request) {
-        return handler(e, request, HttpStatus.BAD_REQUEST);
+    private Object handler(Object error, HttpStatus s) {
+        return ResponseEntity.status(s).body(responseService.error(error, s));
     }
 
     @ExceptionHandler
-    public Object noSuchElementException(NoSuchElementException e, HttpServletRequest request) {
-        return handler(e, request, HttpStatus.NOT_FOUND);
+    public Object handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var error = e.getBindingResult().getFieldErrors().stream()
+                .map(ex -> String.format("%s %s", ex.getField(), ex.getDefaultMessage()))
+                .toList();
+        return handler(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    public Object dataIntegrityViolationException(DataIntegrityViolationException e, HttpServletRequest request) {
-        return handler(e, request, HttpStatus.CONFLICT);
+    public Object handleConstraintViolationException(ConstraintViolationException e) {
+        var error = e.getConstraintViolations().stream()
+                .map(v -> String.format("%s %s", v.getPropertyPath(), v.getMessage()))
+                .toList();
+        return handler(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    public Object exception(Exception e, HttpServletRequest request) {
-        return handler(e, request, HttpStatus.INTERNAL_SERVER_ERROR);
+    public Object handleNoSuchElementException(NoSuchElementException e) {
+        return handler(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public Object handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        var error = Optional.ofNullable(e.getRootCause())
+                .map(Throwable::getMessage)
+                .orElse(e.getMessage());
+        return handler(error, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler
+    public Object handleException(Exception e) {
+        return handler(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
